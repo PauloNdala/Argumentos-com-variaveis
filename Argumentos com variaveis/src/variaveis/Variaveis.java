b104e68e3a5c7be43daa2f8b099b4735c524a51d
PauloNdala <paulondumbandala@gmail.com>
2022-04-03 23:12:57 +0100

    Argumentos com variaveis

---
+++ Argumentos com variaveis/src/variaveis/Variaveis.java
@@ -0,0 +1,16 @@
+package variaveis;
+
+public class Variaveis {
+	
+   private double s;
+   public double soma(String titulo,double...listaNumeros) {
+	   System.out.println(titulo);
+	   double total = 0 ;
+	   for (double n : listaNumeros) {
+		   total+=n;
+	   }
+	   return total;
+   }
+    
+
+}
---
+++ Argumentos com variaveis/src/variaveis/VariaveisTest.java
@@ -0,0 +1,11 @@
+package variaveis;
+
+public class VariaveisTest {
+
+	public static void main(String[] args) {
+		Variaveis p = new Variaveis();
+		double total = p.soma("soma numeros",1,2,3,1);
+		System.out.println("Total = "+total);
+	}
+
+}
