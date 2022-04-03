546de457fa49e894c7f4cdea163d97463d4f6e91
PauloNdala <paulondumbandala@gmail.com>
2022-04-03 23:15:27 +0100

    Modifiquei

--- Argumentos com variaveis/src/variaveis/Variaveis.java
+++ Argumentos com variaveis/src/variaveis/Variaveis.java
@@ -1,7 +1,7 @@
 package variaveis;
 
 public class Variaveis {
-   //Declarando Variveis
+   
    private double s;
    public double soma(String titulo,double...listaNumeros) {
 	   System.out.println(titulo);
